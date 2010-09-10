1b400d2607665eb9a114a7251a48150aaff387ea
Andreas Haller <andreashaller@gmail.com>
2010-09-10 12:23:41 +0200

    updated the generator to work with Rails 3 / Thor
    removed (unused?) templates/test_helper.rb

--- lib/generators/sauron/sauron_generator.rb
+++ lib/generators/sauron/sauron_generator.rb
@@ -1,33 +1,28 @@
-class SauronGenerator < Rails::Generator::Base
-  def manifest
-    options[:workers] ||= 4
+class SauronGenerator < Rails::Generators::Base
+  source_root File.expand_path("../templates", __FILE__)
+  
+  class_option :workers, :type => :numeric, :default => 4, 
+    :desc => "Number of workers (threads/databases used during tests)"
+  class_option :rspec, :type => :boolean, :default => false
+  class_option :testunit, :type => :boolean, :default => true
+  
+  # TODO use hook_for :test_framework ?
+  
+  def manifest    
+    copy_file 'sauron_watchr.rb', 'sauron_watchr.rb'
+    copy_file 'script/sauron', 'script/sauron'
 
-    options[:rspec] ||= false
-    options[:testunit] ||= false
+    copy_file 'config/hydra.yml', 'config/hydra.yml'
+    template 'config/sauron.yml', 'config/sauron.yml'
     
-    options[:framework] = options[:rspec] ? 'rspec' : 'testunit'
-
-    record do |m|
-      m.file 'sauron_watchr.rb', 'sauron_watchr.rb'
-      m.file 'test_helper.rb', 'test_helper.rb'
-      m.file 'script/sauron', 'script/sauron', :chmod => 0755
-
-      m.file 'config/hydra.yml', 'config/hydra.yml'
-      m.template 'config/sauron.yml', 'config/sauron.yml'
-      
-      m.directory 'lib/sauron'
-      m.file 'lib/sauron/watchr.rb', 'lib/sauron/watchr.rb' 
-      
-      m.directory 'lib/tasks'
-      m.file 'lib/tasks/sauron.rake', 'lib/tasks/sauron.rake'
-    end
+    directory 'lib/sauron'
+    
+    directory 'lib/tasks'
   end
   
   protected
   
-  def add_options!(opt)
-    opt.on('--workers='){|v| options[:workers] = v}
-    opt.on('--rspec'){|v| options[:rspec] = v}
-    opt.on('--testunit'){|v| options[:testunit] = v}
+  def test_framework
+    options.rspec ? 'rspec' : 'testunit'
   end
 end
